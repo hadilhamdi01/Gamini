8a16c7fe28ba06483e87a77d22ca61bbc32a112d
hadil hamdi <hadilfb51@gmail.com>
2024-04-20 15:07:39 +0100

    email reset pswd

--- src/register.php
+++ src/register.php
@@ -42,7 +42,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
 
-    <title>My HTML Page</title>
+   
 </head>
 
 
@@ -52,12 +52,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             <div class="circle circle-one"></div>
             <div class="form-container">
                 <img src="https://raw.githubusercontent.com/hicodersofficial/glassmorphism-login-form/master/assets/illustration.png" alt="illustration" class="illustration" />
-                <h1 class="opacity">LOGIN</h1>
+                <h1 class="opacity">REGISTER</h1>
                 <form action="register.php" method="POST">
                     <input type="text"  name="username" placeholder="USERNAME" />
                     <input type="email"  name="email" placeholder="EMAIL" />
                     <input type="password" name="password" placeholder="PASSWORD" />
-                    <button class="opacity" type="submit">Enregistrer</button>
+                    <button class="opacity" type="submit">SIGN UP</button>
                 </form>
               
             </div>
